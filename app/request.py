7b04363610b5fe5b4cb7b449d9877d89cdc6cb89
Kvria <trishakuria@gmail.com>
2020-04-27 15:27:54 +0300

    creating requeast to load data of the news object

--- app/request.py
+++ app/request.py
@@ -12,24 +12,24 @@ base_url = app.config['NEWS_API_BASE_URL']
 
 ...
 
-def get_news(category):
+def get_allnews(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_allnews_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
+    with urllib.request.urlopen(get_allnews_url) as url:
+        get_allnews_data = url.read()
+        get_allnews_response = json.loads(get_allnews_data)
 
-        news_results = None
+        allnews_results = None
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
+        if get_allnews_response['results']:
+            allnews_results_list = get_allnews_response['results']
+            allnews_results = process_results(allnews_results_list)
 
 
-    return news_results
+    return allnews_results
 
 def process_results(news_list):
     '''
