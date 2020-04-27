3f123c3383477910c92cb6845dd2b3e86f89d2d2
Kvria <trishakuria@gmail.com>
2020-04-27 16:11:04 +0300

    add links to init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import DevConfig
+from app import views
+from app import error
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
